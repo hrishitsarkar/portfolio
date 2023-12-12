ba0cf254911d7b160fb85bea257c7452a550c3df
Hrishit <hrishitjohn@gmail.com>
2023-12-12 15:12:38 +0530

    skills started

--- Components/Navbar.tsx
+++ Components/Navbar.tsx
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { FaCode } from "react-icons/fa";
 import { TiThMenu } from "react-icons/ti";
-export const Navbar = () => {
+import { CiDark, CiLight } from "react-icons/ci";
+import Link from "next/link";
+interface NavbarProps {
+  whiteToBlack: string;
+  blackToWhite: string;
+  setWhiteToBlack: (color: string) => void;
+  setBlackToWhite: (color: string) => void;
+}
+
+export const Navbar: React.FC<NavbarProps> = ({
+  whiteToBlack,
+  blackToWhite,
+  setWhiteToBlack,
+  setBlackToWhite,
+}) => {
   const navList: { text: string; link: string }[] = [
     {
       text: "Home",
@@ -9,7 +23,7 @@ export const Navbar = () => {
     },
     {
       text: "My Skills",
-      link: "/skills",
+      link: "#skills",
     },
     {
       text: "My Projects",
@@ -20,24 +34,75 @@ export const Navbar = () => {
       link: "/contact",
     },
   ];
+  const toggleDark = () => {
+    setWhiteToBlack("black");
+    setBlackToWhite("white");
+  };
+  const toggleLight = () => {
+    setWhiteToBlack("white");
+    setBlackToWhite("black");
+  };
   return (
-    <div className="w-full h-[80px] bg-black flex items-center justify-between">
+    <div
+      className={`w-full h-[80px] fixed ${
+        whiteToBlack === "white" ? "bg-white" : "bg-black"
+      } flex items-center justify-between`}
+    >
       <div className="w-[50%] p-5 flex items-center justify-start">
-        <div className="text-white text-2xl px-4">
+        <div
+          className={`${
+            blackToWhite === "black" ? "text-black" : "text-white"
+          } text-2xl px-4`}
+        >
           <FaCode />
         </div>
-        <h1 className="text-white font-semibold text-2xl">Portfolio</h1>
+        <h1
+          className={`${
+            blackToWhite === "black" ? "text-black" : "text-white"
+          } font-semibold text-2xl`}
+        >
+          <span className="text-4xl">H</span>rishit{" "}
+          <span className="text-4xl">S</span>arkar
+        </h1>
       </div>
-      <div className="w-[50%] p-5 flex items-center justify-between text-white font-semibold ">
+      <div
+        className={`w-[40%] p-5 flex items-center  justify-between ${
+          blackToWhite === "black" ? "text-black" : "text-white"
+        } font-semibold`}
+      >
         {navList.map((element, index) => (
-          <span
-            key={index}
-            className="hidden md:block  hover:text-2xl transition-all cursor-pointer rounded-full"
-          >
-            {element.text}
-          </span>
+          <Link href={element.link}>
+            <span
+              key={index}
+              className={`hidden md:block hover:scale-[1.5]  hover:${
+                blackToWhite === "black" ? "text-black" : "text-white"
+              } transition-all cursor-pointer rounded-full`}
+            >
+              {element.text}
+            </span>
+          </Link>
         ))}
       </div>
+      {whiteToBlack === "white" && (
+        <div
+          className={`${
+            blackToWhite === "black" ? "text-black" : "text-white"
+          }}  cursor-pointer shadow-lg flex items-center justify-center p-3 mr-2 text-2xl font-bold rounded-full border-[1px] ${
+            blackToWhite === "black" ? "border-black" : "border-white"
+          }`}
+          onClick={toggleDark}
+        >
+          <CiDark />
+        </div>
+      )}
+      {blackToWhite === "white" && (
+        <div
+          className={`text-${blackToWhite} cursor-pointer shadow-lg flex items-center justify-center p-3 mr-2 text-2xl font-bold rounded-full border-[1px] border-${blackToWhite}`}
+          onClick={toggleLight}
+        >
+          <CiLight />
+        </div>
+      )}
     </div>
   );
 };
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,18 @@
 "use client";
+import { useState } from "react";
 import { Navbar } from "@/Components/Navbar";
 import Link from "next/link";
 import Image from "next/image";
 import { FaExternalLinkAlt } from "react-icons/fa";
 import { FaLinkedin } from "react-icons/fa";
-import { FaGithub, FaNodeJs, FaHtml5, FaCss3, FaJava } from "react-icons/fa";
+import {
+  FaGithub,
+  FaNodeJs,
+  FaHtml5,
+  FaCss3,
+  FaJava,
+  FaBootstrap,
+} from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6";
 import { FaReact } from "react-icons/fa";
 import { IoLogoJavascript } from "react-icons/io";
@@ -19,8 +27,43 @@ import {
   SiPostman,
   SiFirebase,
 } from "react-icons/si";
-import { useState } from "react";
-export default function Home() {
+
+export default function Home(props: any) {
+  const skillsFrontend: { name: string; icon: any }[] = [
+    {
+      name: "React",
+      icon: <FaReact />,
+    },
+    {
+      name: "Next js",
+      icon: <TbBrandNextjs />,
+    },
+    {
+      name: "Redux Toolkit",
+      icon: <SiRedux />,
+    },
+    {
+      name: "Tailwind CSS",
+      icon: <SiTailwindcss />,
+    },
+    {
+      name: "HTML",
+      icon: <FaHtml5 />,
+    },
+    {
+      name: "CSS",
+      icon: <FaCss3 />,
+    },
+    {
+      name: "Javascript",
+      icon: <IoLogoJavascript />,
+    },
+    {
+      name: "Bootstrap",
+      icon: <FaBootstrap />,
+    },
+  ];
+
   const socialLinks: { icon: any; url: string }[] = [
     {
       icon: <FaLinkedin />,
@@ -82,21 +125,37 @@ export default function Home() {
       icon: <FaJava />,
     },
   ];
-  const [color, setColor] = useState<string>("white");
+  const [whiteToBlack, setWhiteToBlack] = useState<string>("white");
+  const [blackToWhite, setBlackToWhite] = useState<string>("black");
   return (
     <>
-      <Navbar color={color} setColor={setColor} />
-      <main className={`flex items-center justify-between bg-${color}`}>
-        <div className="w-[50%] h-[90vh] flex flex-col items-center justify-center p-10  ">
-          <h1 className="text-[3rem] font-bold text-center mt-10">
+      <Navbar
+        whiteToBlack={whiteToBlack}
+        blackToWhite={blackToWhite}
+        setWhiteToBlack={setWhiteToBlack}
+        setBlackToWhite={setBlackToWhite}
+      />
+      <main
+        className={`flex items-center justify-between ${
+          whiteToBlack === "white" ? "bg-white" : "bg-black"
+        }`}
+      >
+        <div className="w-[50%] h-[100vh] flex flex-col items-center justify-center p-10  ">
+          <h1
+            className={`text-[3rem] text-${blackToWhite} font-bold text-center mt-[100px]`}
+          >
             Full Stack Web Developer
           </h1>
-          <div className="flex items-center justify-between w-[70%] text-2xl mt-5">
+          <div
+            className={`flex text-${blackToWhite}  items-center justify-between w-[70%] text-2xl mt-5`}
+          >
             {techLinks.map((element) => (
-              <span>{element.icon}</span>
+              <span className="hover:scale-[1.5] transition-all">
+                {element.icon}
+              </span>
             ))}
           </div>
-          <p className="text-xl m-10 ">
+          <p className={`text-xl m-10 text-${blackToWhite}`}>
             Dedicated Full Stack Web Developer with strong expertise in
             JavaScript, React.js, and Node.js. Experienced in crafting and
             sustaining web applications, leveraging Redux Toolkit for efficient
@@ -106,18 +165,24 @@ export default function Home() {
             of meeting project deadlines and delivering results even in
             high-pressure situations.
           </p>
-          <div className="text-xl  rounded-full border-gray-200 hover:bg-black hover:text-white transition-all border-2 p-2 font-bold hover:shadow-2xl">
+          <div
+            className={`text-xl ${
+              whiteToBlack === "white" ? "bg-white" : "bg-black"
+            } text-${blackToWhite}  rounded-full border-gray-200 hover:bg-sky-600 hover:text-${whiteToBlack} transition-all  p-2 font-bold hover:shadow-2xl`}
+          >
             <Link href="https://drive.google.com/file/d/1GuAWf0oVKZyLHssbkdFS4rKIo2PAwpIF/view?usp=sharing">
               <span className="flex items-center justify-between">
-                <p className="m-1">Resume</p>
+                <p className={`m-1`}>Resume</p>
                 <FaExternalLinkAlt />
               </span>
             </Link>
           </div>
-          <div className="flex mt-10 p-2 items-center justify-between w-[20%] h-[200px]">
+          <div
+            className={`flex mt-10 p-2 text-${blackToWhite} items-center justify-between w-[20%] h-[200px]`}
+          >
             {socialLinks.map((element) => (
               <Link href={element.url}>
-                <span className="flex items-center justify-between text-xl  hover:shadow-2xl transition-all">
+                <span className="flex hover:scale-[1.5] items-center justify-between text-xl  hover:shadow-2xl transition-all">
                   {element.icon}
                 </span>
               </Link>
@@ -132,11 +197,35 @@ export default function Home() {
             width={450}
             className="rounded-full m-10 hover:shadow-2xl transition-all"
           />
-          <div>
-            <h1 className="text-4xl font-semibold">Hrishit Sarkar</h1>
-          </div>
         </div>
       </main>
+      <section
+        id="skills"
+        className={`w-full ${
+          whiteToBlack === "white" ? "bg-white" : "bg-black"
+        } flex flex-col p-5`}
+      >
+        <h1
+          className={`text-center font-bold text-6xl text-${blackToWhite} m-2`}
+        >
+          My Skills
+        </h1>
+        <h1 className={`font-bold text-4xl text-${blackToWhite} m-5 `}>
+          Front-end
+        </h1>
+        <div className="flex items-center justify-between flex-wrap p-5 ">
+          {skillsFrontend.map((element) => (
+            <div className="flex items-center justify-between  flex-col p-5">
+              <span className={`text-9xl text-${blackToWhite}`}>
+                {element.icon}
+              </span>
+              <p className={`text-4xl m-2 text-${blackToWhite}`}>
+                {element.name}
+              </p>
+            </div>
+          ))}
+        </div>
+      </section>
     </>
   );
 }
